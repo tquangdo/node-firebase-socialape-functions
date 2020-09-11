d1588d9a544c6a1948614825447d2bf5e55ef379
tquangdo <trangia61@gmail.com>
2020-09-11 13:37:42 +0900

    20200911 13:37

---
+++ functions/util/config.js
@@ -0,0 +1,12 @@
+//FB > project settings > General > Your apps > Firebase SDK snippet:⦿Config
+//https://console.firebase.google.com/u/0/project/socialape-efcc4/settings/general/web:OGZiNGQwY2MtMjA0Ny00OWNmLWJlY2QtNzg4YzZjYmJkYmUw
+module.exports = {
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx",
+    measurementId: "xxx"
+}
